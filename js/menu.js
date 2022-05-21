bf74459a8306c66f236d64672114461269058eb2
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-05-21 16:48:37 +0300

    z-index modal fixed

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,18 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
 
+  const mobMenuModalBtn = document.querySelector('.mobMenuBtn');
+
+  mobMenuModalBtn.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-hidden');
+    document.body.classList.toggle('fixedBody');
+  });
+
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
@@ -12,3 +24,5 @@
     document.body.classList.toggle('fixedBody');
   });
 })();
+
+function mobileMenuClose() {}
